d206b2a77674b5eebf1448543e04abf28e530430
Manvita <123493308+Manvita11@users.noreply.github.com>
2024-08-23 23:06:02 +0530

    Time: 25 ms (51%), Space: 44.8 MB (76.97%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,28 +1,40 @@
 class Solution {
+    public static int findMax(int[] piles ){
+        int n = piles.length;
+        int max = piles[0];
+
+        for(int i =0 ; i < n ;i++){
+            if(piles[i] > max){
+                max = piles[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static int findTotalHours(int[] piles , int hours){
+        int n = piles.length;
+        int totalHrs = 0;
+        for(int i =0 ;i <  n; i++){
+            totalHrs += Math.ceil((double) piles[i] /(double) hours);       // type casting 
+        }
+
+        return totalHrs;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int high = Arrays.stream(piles).max().getAsInt();
-        int low = 1;
-        int ans = high;
-        while (low <= high) {
-            int mid = (low+high)/2;
-            if (feasible(piles, h, mid)) {
-                ans = mid;
+        int low = 1, high = findMax(piles);
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            int totalHours = findTotalHours(piles , mid);
+            
+            if(totalHours <= h){
                 high = mid - 1;
-            }  
-            else {
+            }else{
                 low = mid + 1;
-            }  
+            }
         }
 
-        return ans;
-    }
-
-    public boolean feasible(int[] piles, int h, int k) {
-        int hours = 0;
-        for (int p : piles) {
-            hours += Math.ceil((double)p/k);
-        }
-
-        return hours <= h;
-    }
+        return low;
+    } 
 }
