9e195f11998b648c4f7ef4a3c6350406b71b388b
Manvita <123493308+Manvita11@users.noreply.github.com>
2024-08-28 23:05:53 +0530

    Time: 17 ms (36.47%), Space: 45.4 MB (64.67%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -2,8 +2,6 @@ class Solution {
     public String frequencySort(String s) {
      StringBuilder sb = new StringBuilder();
         Map<Character, Integer> mp = new HashMap<>();
-        
-        // Traversing in the String to insert character in the map according to their frequency.
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if(mp.isEmpty()){
@@ -17,16 +15,12 @@ class Solution {
             }
         }
 
-        //  Inserting all the element of Map into list.
         List<Map.Entry< Character, Integer>> list = new ArrayList<>(mp.entrySet());
 
-        //  Sorting the List.
         list.sort(Map.Entry.comparingByValue());
 
-        // Reversing the list in order to get the most frequent character in the start.
         Collections.reverse(list);
 
-        //  Traversing in the list and inserting character in the Stringbuilder according to their frequency.
         for (Map.Entry<Character, Integer> entry : list) {
             int a = entry.getValue();
             char c = entry.getKey();
