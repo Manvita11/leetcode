193418af67963e8b02a316477f20b05f9abde966
Manvita <123493308+Manvita11@users.noreply.github.com>
2024-09-06 00:18:20 +0530

    Time: 4 ms (100%), Space: 43.7 MB (79.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x == 0) return true;
-
-        if(x < 0 || x % 10 == 0) return false;
-
-        int reverse = 0;
-        while(x > reverse){
-            int pop = x%10;
-            x /= 10;
-
-            reverse = (reverse * 10) + pop;
-        }
-        if(x == reverse || x == reverse / 10){
-            return true;
-        }else{
+        if (x < 0) {
             return false;
         }
+
+        int reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == xcopy;        
     }
 }
+       
