0e73064c9c5fd829f59d0e2ce9a7ed4ac887697a
Manvita <123493308+Manvita11@users.noreply.github.com>
2024-09-12 00:05:41 +0530

    Time: 1 ms (100%), Space: 45.2 MB (30.79%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -1,44 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        if(head.next == null)
-            return head;
-
-        ListNode temp = head;
-        while(temp.next != null)
-        {
-            putInside(temp,greatestCommonDivisor(temp.val,temp.next.val));
-            temp = temp.next.next;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            int value = findGCD(temp.val,temp.next.val);
+            ListNode curr = new ListNode(value);
+            curr.next=temp.next;
+            temp.next=curr;
+            temp = curr.next;
         }
         return head;
     }
-
-    public int greatestCommonDivisor(int num1, int num2)
-    {
-        int high,low;
-        if(num1 > num2){
-            high = num1;
-            low = num2;
-        }
-        else{
-            high = num2;
-            low = num1;
-        }
-        int low2 = low;
-        while(low>1){
-            if(high % low !=0 ||  low2%low !=0)
-            {
-                low--;
-            }
-            else{
-                return low;
-            }
-        }
-        return 1;
-    }
-    public void putInside(ListNode head, int num)
-    {
-        ListNode temp = head.next;
-        head.next = new ListNode(num);
-        head.next.next = temp;
+    private int findGCD(int a,int b){
+       if(b==0){
+        return a;
+       }
+      return findGCD(b,a%b);
     }
 }
