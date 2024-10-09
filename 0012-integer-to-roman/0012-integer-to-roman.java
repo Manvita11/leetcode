548d0e8c12fbe356b333e2ce7669d0a8935a9fad
Manvita <123493308+Manvita11@users.noreply.github.com>
2024-10-09 23:05:25 +0530

    Time: 3 ms (98.31%), Space: 44.6 MB (34.92%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,63 +1,80 @@
 class Solution {
-    static HashMap <Integer, String> map = new HashMap<>();
-
-    public static String roman(int tem, int i){
-        int powOf5 = 5 *((int)Math.pow(10, i));
-        int powOf10 = ((int)Math.pow(10, i));
-
-        if (map.containsKey(tem)){
-            return map.get(tem);
-        }
-
-        String s ="";
-        if(powOf5 < tem){
-            s+= map.get(powOf5); 
-            tem-= powOf5; 
-        }
-
-        while (tem != 0) {
-            s +=  map.get(powOf10);
-            tem -= powOf10;
-        }
-
-        i=s.length()-1;
-        String ans ="";
-        while(i>=0){
-            ans += s.charAt(i);
-            i--;
-        }
-        return ans;
-    }
     public String intToRoman(int num) {
-        String s = "";
-        int i=0, tem =0;
-        map.put(1, "I");
-        map.put(5, "V");
-        map.put(10, "X");
-        map.put(50, "L");
-        map.put(100, "C");
-        map.put(500, "D");
-        map.put(1000, "M");
-        map.put(4, "VI");
-        map.put(9, "XI");
-        map.put(40, "LX");
-        map.put(90, "CX");
-        map.put(400, "DC");
-        map.put(900, "MC");
-
-        while (num != 0) {
-            tem = num%10;
-            s+=roman((int)(tem*Math.pow(10, i)), i);
-            num /= 10;
-            i++;
+        int rem,src=0;
+        StringBuilder roman=new StringBuilder();
+        while(num>0){
+            rem=num%10;
+            num=num/10;
+            if(src==0){
+                if(rem==9){
+                    roman.insert(0,"IX");
+                    rem-=9;
+                }
+                else if(rem>=5){
+                    roman.insert(0,"V");
+                    rem-=5;
+                }
+                else if(rem==4){
+                    roman.insert(0,"IV");
+                    rem-=4;
+                }
+                while(rem!=0){
+                    roman.insert(roman.length(),"I");
+                    rem-=1;
+                }
+            }
+            else if(src==1){
+                if(rem==9){
+                    roman.insert(0,"XC");
+                    rem-=9;
+                }
+                else if(rem>=5){
+                    roman.insert(0,"L");
+                    rem-=5;
+                    while(rem!=0){
+                        roman.insert(1,"X");
+                        rem-=1;
+                    }
+                }
+                else if(rem==4){
+                    roman.insert(0,"XL");
+                    rem-=4;
+                }
+                while(rem!=0){
+                    roman.insert(0,"X");
+                    rem-=1;
+                }
+            }
+            else if(src==2){
+                if(rem==9){
+                    roman.insert(0,"CM");
+                    rem-=9;
+                }
+                else if(rem>=5){
+                    roman.insert(0,"D");
+                    rem-=5;
+                    while(rem!=0){
+                        roman.insert(1,"C");
+                        rem-=1;
+                    }
+                }
+                else if(rem==4){
+                    roman.insert(0,"CD");
+                    rem-=4;
+                }
+                while(rem!=0){
+                    roman.insert(0,"C");
+                    rem-=1;
+                }
+            }
+            else{
+                while(rem!=0){
+                    roman.insert(0,"M");
+                    rem-=1;
+                }
+            }
+            src++;
         }
-
-        i=s.length()-1;
-        String ans ="";
-        while(i>=0){
-            ans += s.charAt(i);
-            i--;
-        }
-        return ans;
+        return roman.toString();
     }
 }
